d5b02c98bf25496e5d142d78aa57bd26b5d5d0ba
theArctesian <stephen.d.okita@gmail.com>
2023-02-07 23:38:39 +0800

    added articles

---
+++ src/components/blogEntery.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import "../../app.css";
+  export let title: String;
+  export let date: String;
+  export let content: String;
+</script>
+
+<div class=" flex flex-col">
+  <div class="head flex flex-col">
+    <h1 class="text-2xl">Title: {title}</h1>
+    <h1 class="text-xl">Date: {title}</h1>
+    <p class="text-xl">{content}</p>
+  </div>
+</div>
+
+<style>
+  .head:hover {
+    border: 0.2rem solid var(--red);
+  }
+  .head {
+    color: var(--black);
+    border-radius: 1rem;
+    padding: 1rem;
+    border: 0.2rem solid var(--fg);
+  }
+  h1 {
+    font-weight: bold;
+  }
+  .text {
+    background-color: var(--fg);
+    padding: 1rem;
+    text-align: center;
+    border-radius: 1rem;
+    font-size: small;
+    margin: 1rem;
+  }
+</style>
--- src/routes/blog/writings/+page.svelte
+++ src/routes/blog/writings/+page.svelte
@@ -1,23 +1,32 @@
 <script>
-    import "../../../app.css";
-    import Typewriter from "svelte-typewriter";
+  import "../../../app.css";
+  import Typewriter from "svelte-typewriter";
+  import writings from "./writings.json";
 </script>
 
-<div class="text">
-    <p>
-        This is still a work in progress<Typewriter mode="loop" interval="100"
-            >...</Typewriter
-        >
-    </p>
+<div class="flex flex-col">
+  {#each writings as i}
+    <div class=" text flex flex-col">
+      <div class="text-xl text-center">
+        <h1>{i.title}</h1>
+        <h1 class="text-l">Date: {i.date}</h1>
+      </div>
+      <br>
+      <p class="text-m float-left">{i.content}</p>
+    </div>
+  {/each}
 </div>
 
 <style>
-    .text {
-        background-color: var(--fg);
-        padding: 1rem;
-        text-align: center;
-        border-radius: 1rem;
-        font-size: small;
-        margin: 1rem;
-    }
+  .text {
+    background-color: var(--fg);
+    padding: 1rem;
+    border-radius: 1rem;
+    font-size: small;
+    margin: 1rem;
+  }
+
+  h1 {
+    font-weight: bold;
+  }
 </style>
