61e53b4860f2f4aecfc1fba348e2a5cced33aa61
theArctesian <stephen.d.okita@gmail.com>
2024-02-08 16:52:19 -0800

    speed analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
     inject({ mode: dev ? 'development' : 'production' });
+    import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 
 </script>
 
